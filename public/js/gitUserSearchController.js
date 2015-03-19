faf8ccc635e1dd509ae67072fa66b0bbd7aac01a
Sean Haughton <seanphaughton@gmail.com>
2015-03-19 12:02:59 +0000

    Pre-heroku refactoring

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,13 +1,11 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
   var searchResource = $resource('https://api.github.com/search/users');
-  var key = key;
 
   $scope.doSearch = function() {
     
     $scope.searchResult = searchResource.get({
-      q: $scope.searchTerm,
-      access_token: key
+      q: $scope.searchTerm
     });
 
   };
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.render('index')
 });
 
-server.listen(3000, function() {
-  console.log("Server listening on port 3000");
+server.listen(port, function() {
+  console.log("Server listening on http://localhost:" + port);
 });
 
 module.exports = server;
